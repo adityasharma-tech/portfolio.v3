53f1ffb983b8a389f96653edb43683d6ddaf2a97
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-02-20 13:04:54 +0530

    update

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HeroSection from "./home/hero-section";
 import ProjectSection from "./home/projects-section";
 import { Switch } from "radix-ui";
@@ -6,16 +6,24 @@ import LearningSection from "./home/learning-section";
 import Footer from "./home/footer";
 
 export default function Home() {
+  const [designMode, setDesignMode] = useState(false);
+  React.useLayoutEffect(() => {
+    if (designMode) window.document.designMode = "on";
+    else window.document.designMode = "off";
+  }, [designMode]);
   return (
     <React.Fragment>
-      <div className="sticky top-10 z-50 flex justify-end">
-        <div className="gap-x-2 bg-amber-200 md:inline-flex hidden rounded-full pl-2 text-neutral-900 text-sm items-center">
+      <div className="sticky top-10 z-50 flex justify-between">
+        <div className="flex items-center gap-x-3 bg-black/10 px-5 h-10 backdrop-blur-sm">
+          <span className="font-medium">Aditya Sharma</span>
+          <div className="border-l-2 border-neutral-400 h-full" />
+          <span className="font-medium">Freelancer</span>
+        </div>
+        <div onClick={()=>setDesignMode(!designMode)} className="gap-x-2 bg-amber-200 cursor-pointer md:inline-flex hidden rounded-full my-auto pl-2 text-neutral-900 text-sm items-center">
           Design mode{" "}
           <Switch.Root
-            onCheckedChange={(checked) => {
-              if (checked) document.designMode = "on";
-              else document.designMode = "off";
-            }}
+            checked={designMode}
+            onCheckedChange={setDesignMode}
             className="SwitchRoot my-auto"
             id="airplane-mode"
           >
@@ -25,8 +33,8 @@ export default function Home() {
       </div>
       <HeroSection />
       <ProjectSection />
-      <LearningSection/>
-      <Footer/>
+      <LearningSection />
+      <Footer />
     </React.Fragment>
   );
 }
