cf936420f8a5f89f65f176a9a596ac6cc07cc3b2
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-02-27 21:22:19 +0530

    Create test.tsx

---
+++ frontend/src/app/test/test.tsx
@@ -0,0 +1,11 @@
+export default function Test(){
+  return (
+    <div>
+      <span>Instagram Post</span>
+    <instagram-post
+  post-url="https://www.instagram.com/p/DFVRxXIS9KJ/"
+  captions="true"
+></instagram-post>
+    </div>
+    )
+}
