0a31c0a14e8eefa135f88b4dde76369efe27592b
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-02-27 21:25:51 +0530

    Update test.tsx

--- frontend/src/app/test/test.tsx
+++ frontend/src/app/test/test.tsx
@@ -2,10 +2,8 @@ export default function Test(){
   return (
     <div>
       <span>Instagram Post</span>
-    <instagram-post
-  post-url="https://www.instagram.com/p/DFVRxXIS9KJ/"
-  captions="true"
-></instagram-post>
+      <div className="w-20 h-36" dangerouslySetInnerHTML={{__html: "<instagram-postpost-url="https://www.instagram.com/p/DFVRxXIS9KJ/"captions="true"></instagram-post>"}}></div>
+    
     </div>
     )
 }
